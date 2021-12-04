fb7107992a02b60a265901dff0fdcc1e4f6c1e74
Kaleb Olson <kalebroyolson@gmail.com>
2021-12-04 17:42:54 -0600

    Fully functional UI, no backend

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 import { useState } from 'react'
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
         id: 1,
@@ -30,14 +32,40 @@ function App() {
     },
 ])
 
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 10000) +1
+  const newTask = { id, ...task }
+  setTasks([...tasks, newTask])
+}
+
 const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !== id))
 }
 
+const toggleReminder = (id) => {
+  setTasks(
+    tasks.map((task) => 
+    task.id === id ? {...task, reminder: !task.reminder} : task))
+}
+
+const toggleAddTask = () => {
+  setShowAddTask(!showAddTask)
+}
+
+  //the && is just a ternary without an else
   return (
     <div className="container">
-        <Header title= "Task Tracker" />
-        <Tasks tasks={tasks} onDelete={deleteTask}/>
+        <Header title= "Task Tracker" 
+        onShowAddTaskToggle={toggleAddTask} 
+        showAdd={showAddTask}/>
+        {showAddTask && <AddTask onAdd={addTask}/>} 
+        {tasks.length > 0 
+        ? <Tasks 
+            tasks={tasks} 
+            onDelete={deleteTask}
+            onToggle={toggleReminder}
+          /> 
+        : ('No tasks to show')}
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,17 @@ import PropTypes from 'prop-types'
 import Button from './Button'
 
 
-const Header = ({title}) => {
-    const onClick = () => {
-        
-    }
+const Header = ({ title, onShowAddTaskToggle, showAdd }) => {
+
 
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button color='green' text='Add' onClick={onClick}/>
+            <Button 
+            color={!showAdd ? 'green' : 'gray'}
+            text={!showAdd ? 'Add' : 'Close'} 
+            onClick={onShowAddTaskToggle}
+            />
         </header>
     )
 }
